cb658fe18a378f071023e069e4861ef00832909b
Shweta Borate <52259989+shwetaborate@users.noreply.github.com>
2025-05-05 17:46:26 +0530

    Update WebDriverSetup.java

--- SaucedemoLogin/src/main/java/com/saucedemo/utils/WebDriverSetup.java
+++ SaucedemoLogin/src/main/java/com/saucedemo/utils/WebDriverSetup.java
@@ -1,10 +1,28 @@
 package com.saucedemo.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverSetup {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private static WebDriver driver;
 
-	}
+    public static WebDriver initializeDriver() {
+        // Set the path to the GeckoDriver executable
+        System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
 
+        // Initialize the FirefoxDriver
+        driver = new FirefoxDriver();
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
